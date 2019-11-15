9d3aa8da1e5b84f728a6fcc41d7e236e45f8d78f
mohamed waali radji <mohwaali@gmail.com>
2019-11-15 22:20:16 +0100

    Create model.php

---
+++ model.php
@@ -0,0 +1,78 @@
+
+<?php
+
+
+
+
+class Model extends DatabaseConnect{
+
+     
+
+     public function mvc_add_data($table , $array_data = array() )
+
+         {
+ 
+            $key = array();
+            $val = array();
+
+            foreach ($array_data as $k => $v) {
+
+                 $key[count($key)] =   $k  ;
+                 $val[count($val)] =   $v  ;
+
+            }
+
+             $key = str_replace("," , "" ,$key) ;
+             $val = str_replace(array(",","'",'"') , array("","\'",'\"') ,$val) ;
+            
+             $key =  "`".implode("`,`", $key ). "`" ;
+             $val = "'". implode("','", $val ) . "'";
+             $val =  htmlentities( $val  ) ;
+
+            $resulte = $this->mysqli->query(" INSERT INTO `$table` ($key) VALUES ($val) " );
+         
+            if($resulte == true)
+           {
+               return true ;
+           }
+                
+          
+
+
+         }
+
+
+     public function mvc_get_data($table = null, $text = null){
+
+        
+
+         $resulte = $this->mysqli->query(sprintf(" select * from `$table` %s",$text));
+          
+             if($resulte == false)
+                 return null;
+          
+                 $num = $resulte->num_rows;
+          
+                     $resuelt = array();
+          
+                         for($i=0;$i<$num;$i++)
+                         {
+
+                             $resuelt[count($resuelt)] = $resulte->fetch_object(); 
+
+                         }
+          
+          
+             return $resuelt;
+                 
+             
+       
+        
+
+    }       
+
+
+}
+
+
+?>
